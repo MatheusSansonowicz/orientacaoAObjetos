7e85ddaa3f191e18df17e098811c8d123daa4532
MatheusSansonowicz <128191037+MatheusSansonowicz@users.noreply.github.com>
2024-06-18 17:12:01 -0300

    Update Pconta.java

--- aula1106/src/exemplosConta/Pconta.java
+++ aula1106/src/exemplosConta/Pconta.java
@@ -5,12 +5,17 @@ public class Pconta {
 	public static void main(String[] args) throws depositoNegativo, semSaldo, saqueNegativo {
 		// TODO Auto-generated method stub
 		
-		contaConcreta C = new contaConcreta(5000, 900);
-		C.depositar(-900);
-		C.depositar(900);
-		C.sacar(-800);
-		C.sacar(10000);
-		C.sacar(2000);
+	try {
+            ContaConcreta conta = new ContaConcreta(5000, 900);
+            conta.depositar(-900);
+            conta.depositar(900);
+            conta.sacar(-800);
+            conta.sacar(10000);
+            conta.sacar(2000);
+            System.out.println("Operações realizadas com sucesso. Saldo atual: " + conta.getSaldo());
+        } catch (DepositoNegativo | SemSaldo | SaqueNegativo e) {
+            System.out.println(e.getMessage());
+        }
 
 	}
 
